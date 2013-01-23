ba8609d5e51cbd1fc86e259800221fa74ee309bd
Kane Rogers <kane.rogers@bluereef.com.au>
2013-01-23 18:40:10 +1100

    INITIAL COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9c9557cd718816bf1505eb67dd9ed871a318663fadbc53fdb0a589d476871c92ee3a5d48a8bdbaa5a77a9767b6dc63d0eea296000272294f09e679ff6e97405d'
