05a0ac6239a0edf2e03b228056123bd217e7f05e
Kane Rogers <kane.rogers@bluereef.com.au>
2013-01-31 21:02:37 +1100

    Adding routes and users stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
   root to: 'static_pages#home'
+
+  resources :users
 
   match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
